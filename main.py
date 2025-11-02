832333ce443b764e840bbe6b18970052387da50a
Frank Carvalho <frank.faculdadeadm@gmail.com>
2025-11-02 14:52:19 -0500

    adição de novos campos no formulário

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import webview
 
+ALTURA = 600
+LARGURA = 1200
+
 class Api:
     pass
 
 if __name__ == "__main__":
     api = Api()
-    janela = webview.create_window("Controle Financeiro Pessoal", "index.html", js_api=api)
+    janela = webview.create_window("Controle Financeiro Pessoal", "index.html", js_api=api, width=LARGURA, height= ALTURA)
     webview.start()
